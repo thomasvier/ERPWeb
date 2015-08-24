d6246568b0842f3ea0d79154db1d80bffa7746fc
thomasvier <thomasvier@gmail.com>
2015-08-24 16:49:21 -0300

    Comit

---
+++ ERPWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ERPWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ERPWeb/Models/Adminstracao/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERPWeb.Models.Adminstracao
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        
+        [Required(ErrorMessageResourceType = typeof(Resource),
+                    ErrorMessageResourceName = "NomeObrigatorio",
+                    AllowEmptyStrings = false)]
+        [StringLength(300)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Resource),
+                    ErrorMessageResourceName = "UsuarioObrigatorio",
+                    AllowEmptyStrings = false)]
+        [StringLength(50)]
+        public string Login { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Resource),
+                    ErrorMessageResourceName = "SenhaObrigatoria",
+                    AllowEmptyStrings = false)]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [EmailAddress(ErrorMessage="Informe um e-mail válido")]
+        [Display(Name="E-mail")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
